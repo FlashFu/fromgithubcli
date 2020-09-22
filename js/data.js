c530b49e3f2ea421c7ba7933f498011ff411b47f
FlashFu <forge451@gmail.com>
2020-09-22 14:25:30 -0500

    new files added

---
+++ js/data.js
@@ -0,0 +1,19 @@
+var diseases = [
+    {
+        'id': 1,
+        'type':'COD',
+        'name': 'stroke',
+        'causes' : [],
+        'risk reducers' : [1]
+    }
+];
+
+
+var effectors = [
+    {
+        'id' : 1,
+        'name' : 'fiber',
+        'sources' : [],
+        'references' : ['http://nutritionfacts.org/video/best-foods-to-reduce-stroke-risk/']
+    }
+];
